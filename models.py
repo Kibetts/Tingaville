6c7b2945b755a15af63a6ff702a20544eabfbb1e
Brian Kibet <kibetsbrian@gmail.com>
2024-04-05 23:50:50 +0300

    create user model

--- models.py
+++ models.py
@@ -6,3 +6,13 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import backref
 from flask_migrate import Migrate
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'  
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(20), default='student')  # Default role is 'student'
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
