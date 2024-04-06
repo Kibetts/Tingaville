202faeb2d0ab167dc10e5c64f7ace57745622c94
Brian Kibet <kibetsbrian@gmail.com>
2024-04-06 15:23:19 +0300

    add app imports

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.orm import Session
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from models import *
+import bcrypt
