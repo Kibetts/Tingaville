a70646b4e6f813ace79e6528abc6a9add5b99021
Brian Kibet <kibetsbrian@gmail.com>
2024-04-06 15:24:13 +0300

    add app configurations

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from models import *
 import bcrypt
+
+session = Session()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
